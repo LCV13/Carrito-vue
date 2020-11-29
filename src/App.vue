a1151097ae804ea7af90472b394be17494f84216
Catalina Villarreal <58574759+LCV13@users.noreply.github.com>
2020-11-29 07:25:21 -0300

    Se importa el componente de Producto

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="row">
+      <div class="col-md-7">
+        <div class="row">
+          <div class="col-md-6">
+            <producto/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Producto from './components/Producto.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+     Producto 
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
