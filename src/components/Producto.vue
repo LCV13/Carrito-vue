61e77fe3fc9f3079e18590f04e509ff264382152
Catalina Villarreal <58574759+LCV13@users.noreply.github.com>
2020-11-29 07:24:17 -0300

    Se crea el componente Producto

---
+++ src/components/Producto.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card my-5">
+        <div class="card-body">
+            <h5 class="text-center card-title">Titulo</h5>
+            <img src="">
+            <p>Precio</p>
+            <button class="btn btn-primary form-control">Agregar al carrito</button>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Producto'
+}
+</script>
+
+
+<style>
+
+</style>
