9caa380f4a75b40c652016c8469ac073c7027b5c
Catalina Villarreal <58574759+LCV13@users.noreply.github.com>
2020-11-29 07:53:05 -0300

    Se crea y muestra el componente Carrito

---
+++ src/components/Carrito.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <ul class="list-group">
+            <li class="list-group-item">
+                Titulo - $Precio
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Carrito'
+}
+</script>
