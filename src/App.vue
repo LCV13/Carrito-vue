aa068b046d8096a3b4017e9088a383128c51f23a
Catalina Villarreal <58574759+LCV13@users.noreply.github.com>
2020-11-29 07:42:59 -0300

    Se muestran los productos

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
+
     <div class="row">
       <div class="col-md-7">
         <div class="row">
-          <div class="col-md-6">
-            <producto/>
+          <div class="col-md-6" v-for="prod in productos" :key="prod.id">
+            <producto :producto="prod"></producto>
           </div>
         </div>
       </div>
     </div>
+
   </div>
 </template>
 
 <script>
+import productos from './productos.json'
 import Producto from './components/Producto.vue'
 
 
@@ -20,6 +23,11 @@ export default {
   name: 'App',
   components: {
      Producto 
+  }, 
+  data(){
+    return{
+      productos
+    }
   }
 }
 </script>
--- src/components/Producto.vue
+++ src/components/Producto.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="card my-5">
         <div class="card-body">
-            <h5 class="text-center card-title">Titulo</h5>
-            <img src="">
-            <p>Precio</p>
+            <h5 class="text-center card-title">{{ producto.titulo }}</h5>
+            <img :src="producto.imagen" width="310" height="150">
+            <p class="text-muted card-text">$ {{ Number(producto.precio).toFixed() }}</p>
             <button class="btn btn-primary form-control">Agregar al carrito</button>
         </div>
     </div>
@@ -12,7 +12,8 @@
 
 <script>
 export default {
-    name: 'Producto'
+    name: 'Producto',
+    props: ['producto']
 }
 </script>
 
